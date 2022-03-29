3b63c89d6986ce89e1b03057c28fc9c678865764
Eduardo Vilke <eduardo@twoweb.com.br>
2022-03-29 00:51:36 -0300

    add url api production

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333/api'
+  baseURL: process.env.NODE_ENV === 'production'
+  ? 'https://eduardo-poke-api.herokuapp.com/api/'
+  : 'http://localhost:3333/api'
 })
