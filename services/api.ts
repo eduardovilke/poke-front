87d307747ee75dd34893c70c91adf69e3d658556
Eduardo Vilke <eduardo.svilke@gmail.com>
2023-11-11 21:18:46 -0300

    refactor: move baseUrl to process.env

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,4 @@
 import axios from 'axios';
-
 export const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'production'
-  ? 'https://eduardo-poke-api.herokuapp.com/api/'
-  : 'http://localhost:3333/api'
+  baseURL: process.env.POKEMON_API
 })
